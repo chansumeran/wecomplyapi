91f37aee1e3a4b8a7aecf1df794fcf09120c5e45
Christian <chrisumerandeveloper@gmail.com>
2023-12-18 00:23:32 +0800

    Sanction Controller Added

--- src/main/java/com/WeComply/WeComplyBackend/controller/SanctionController.java
+++ src/main/java/com/WeComply/WeComplyBackend/controller/SanctionController.java
@@ -1,2 +1,32 @@
-package com.WeComply.WeComplyBackend.controller;public class SanctionController {
+package com.WeComply.WeComplyBackend.controller;
+
+import com.WeComply.WeComplyBackend.dto.SanctionUpdateRequest;
+import com.WeComply.WeComplyBackend.entity.Sanction;
+import com.WeComply.WeComplyBackend.service.SanctionServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/config")
+@RequiredArgsConstructor
+public class SanctionController {
+
+    private final SanctionServiceImpl sanctionService;
+
+    @PutMapping("/update-descriptions")
+    public void updateSanctionDescription(
+            @RequestBody List<SanctionUpdateRequest> updateRequests) {
+        sanctionService.updateSanctionDescriptions(updateRequests);
+    }
+
+    @PutMapping("/update-triggers")
+    public void updateSanctionTrigger(
+            @RequestBody List<SanctionUpdateRequest> updateRequests) {
+        sanctionService.updateSanctionTriggers(updateRequests);
+    }
+
 }
--- src/main/java/com/WeComply/WeComplyBackend/dto/SanctionUpdateRequest.java
+++ src/main/java/com/WeComply/WeComplyBackend/dto/SanctionUpdateRequest.java
@@ -1,2 +1,14 @@
-package com.WeComply.WeComplyBackend.dto;public class SanctionUpdateRequest {
+package com.WeComply.WeComplyBackend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class SanctionUpdateRequest {
+    private Integer sanctionId;
+    private Integer newTrigger;
+    private String newDescription;
 }
