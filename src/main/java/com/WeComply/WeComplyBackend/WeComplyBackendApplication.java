3a31f8962a1ddb8da1e6f045df096139d6655e1a
Christian <chrisumerandeveloper@gmail.com>
2023-12-17 11:25:03 +0800

    Initial project structure.

---
+++ src/main/java/com/WeComply/WeComplyBackend/WeComplyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.WeComply.WeComplyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeComplyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeComplyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WeComply/WeComplyBackend/WeComplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeComply.WeComplyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeComplyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
